a5bdbcb71558b0deeab7e4f4bcfe5c78748952f0
bruno-reload <tcp2.portal@gmail.com>
2023-08-24 00:18:32 -0300

    ballistic launch completed

--- test/Assets/temp/foguete/Script/Booster.cs
+++ test/Assets/temp/foguete/Script/Booster.cs
@@ -5,24 +5,60 @@ using UnityEngine;
 public class Booster : MonoBehaviour
 {
     private const  float gravity = -9.8f;
-    private float angle;
 
-    private float v_y;
-    private float v_x;
+    private float rocketAngle = 0.0F;
+    private float rotationY = 0.0F;
+    private float planeAngle = 30.0f;
 
-    public float speed;
+    private float componentX;
+    private float componentY;
+    private float componentZ;
 
-    private float time;
+    public float speed = 2.0f;
+
+    private float tg;
+
+    private float mSin;
+    private float mCos;
+
+
+    private float spaceX;
+    private float spaceY;
+
+    private float ModuleSpaceX;
+    private float ModuleSpaceY;
+
+    private float deltaTime;
+    private float time = 0.0f;
+
+    private Rigidbody rigidBody;
     void Start()
     {
-        v_y = speed * Mathf.Sin(angle);
-        v_x = speed * Mathf.Cos(angle);
+        rigidBody = GetComponent<Rigidbody>();
+
+        rocketAngle = transform.rotation.eulerAngles.z + 90;
+        rotationY = transform.rotation.eulerAngles.y;
+
+
+        componentY = speed * Mathf.Sin(rocketAngle * Mathf.Deg2Rad);
+        componentX = speed * Mathf.Cos(rocketAngle * Mathf.Deg2Rad);
+
     }
 
-    // Update is called once per frame
     void Update()
     {
         time += Time.deltaTime;
-        
+
+
+        float higth = componentY * time + gravity * Mathf.Pow(time, 2.0f) / 2.0f;
+        float distance = componentX * time;
+
+        Vector3 v = new Vector3(distance, higth, 0.0f);
+
+        Quaternion rotation = Quaternion.AngleAxis(rotationY, Vector3.up);
+
+        rigidBody.velocity = rotation * v;
+
+        transform.up = rigidBody.velocity.normalized;
     }
 }
